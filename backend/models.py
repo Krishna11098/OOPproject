d3677a01d232434161a76f1327583383f2ab2c51
DarthKitten <darthkitten2228@gmail.com>
2025-10-02 11:25:49 +0530

    created announcement model

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,18 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 from database import Base
+
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True) 
-    username = Column(String(50), unique = True)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True)
     email = Column(String(50), unique=True, index=True)
     password = Column(String(1000))
+
+
+class Announcement(Base):
+    __tablename__ = 'announcements'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100))
+    content = Column(String(1000))
+    post_date = Column(Date)
